a3662dc1db1af7d2789c0b512d2004c3330449d2
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-10 19:27:45 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,67 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Light weight and Fast',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Trusted and Reliable',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Layered services',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Horizontal scaling',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Turbine Protocol',
+  },
+];
+
+export const startingFeatures = [
+  'Register to our website by cick on the sign up button',
+  'Login to your dashboard and get your referral link',
+  'Start referring and earn commision on each successful refer.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
